23036b3f7c7fd9d5b8d3577d4bd69f84810af73f
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-24 17:39:54 -0800

    SQL with Flask, student creation form

---
+++ hackbright-web.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, render_template
+
+import hackbright
+
+app = Flask(__name__)
+
+
+@app.route("/student")
+def get_student():
+    """Show information about a student."""
+
+    github = request.args.get('github', 'jhacks')
+    first, last, github = hackbright.get_student_by_github(github)
+    html = render_template("student_info.html",
+        first=first,
+        last=last,
+        github=github)
+    return html
+
+
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student."""
+
+    return render_template("student_search.html")
+
+
+@app.route("/new_student")
+def new_student():
+    """Add a student."""
+
+    return render_template("student_add.html")
+
+
+@app.route("/student-add", methods=['POST'])
+def student_add():
+    """Add a student."""
+    first_name = request.form.get('firstname')
+    last_name = request.form.get('lastname')
+    github = request.form.get('github')
+
+    hackbright.make_new_student(first_name, last_name, github)
+
+    return render_template("student_confirm.html",
+        github=github)
+
+
+if __name__ == "__main__":
+    hackbright.connect_to_db(app)
+    app.run(debug=True)
